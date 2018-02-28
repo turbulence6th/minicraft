a99a81889d0ed2154563636949739689b1af7894
turbulence6th <turbulence6th@gmail.com>
2018-02-28 13:50:42 +0300

    x

--- minicraft/__init__.py
+++ minicraft/__init__.py
@@ -1,18 +1,18 @@
 from map import *
 
-map = Map(1000,1000)
+map = Map(300,300)
 
-for i in range(3000):
+for i in range(300):
     map.addSquareRandomObject(Item('stone'), 3, 10)
-for i in range(3000):
+for i in range(300):
     map.addSquareRandomObject(Item('iron'), 3, 10)
-for i in range(3000):
+for i in range(300):
     map.addSquareRandomObject(Item('wood'), 3, 10)
-for i in range(3000):
+for i in range(300):
     map.addSquareRandomObject(Item('dirt'), 3, 10)
-for i in range(1000):
+for i in range(100):
     map.addSquareRandomObject(Item('gold'), 3, 10)
-for i in range(8000):
+for i in range(800):
     map.addObjectRandom(Animal('chicken'))
-for i in range(8000):
+for i in range(800):
     map.addObjectRandom(Animal('sheep'))
